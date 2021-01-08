dd8d51912252571df90abbc37d521fba5b744ff9
dario617 <spam.dario@gmail.com>
2021-01-08 10:58:59 -0300

    Correctly import legacy.js to learner dashboard

--- lms/static/js/eol-legacy.js
+++ lms/static/js/eol-legacy.js
@@ -302,3 +302,12 @@ var edx = edx || {};
     });
   };
 })(jQuery, gettext, Logger, accessible_modal, interpolate);
+
+$(document).ready(function () {
+  edx.dashboard.legacy.init({
+    dashboard: "${reverse('dashboard') | n, js_escaped_string}",
+    signInUser: "${reverse('signin_user') | n, js_escaped_string}",
+    changeEmailSettings:
+      "${reverse('change_email_settings') | n, js_escaped_string}",
+  });
+});
